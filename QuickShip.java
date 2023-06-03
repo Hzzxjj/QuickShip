f68d3c4aa67ef7cad973d8754b792c5e7cba2e85
Hzzxjj <114772570+Hzzxjj@users.noreply.github.com>
2023-06-03 05:19:55 +0300

    Update QuickShip.java

--- QuickShip.java
+++ QuickShip.java
@@ -347,7 +347,7 @@ public class QuiqShip {
   private static void updateAccount() {
         System.out.println("Enter your username: ");
         String name = scanner.nextLine();
-        while (!checkUserIsExists(name)) {
+        while (!checkUserIsExists(username)) {
             System.out.println("This username not found. Please enter another username:");
             username = scanner.nextLine();
         }
